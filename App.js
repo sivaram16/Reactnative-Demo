ad779442622aab6c9cd9a47197734692a74cecf0
ThalapathySiva <sivaramsiva16@gmail.com>
2018-12-27 22:57:15 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is a Demo Application Using React Native</Text>
+        <Text> For </Text>
+        <Text> Arthur Tkachenko </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
